7b0bdf1620d0a9da8b626824b688d4e27ec5888b
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-01 21:46:40 -0300

    feat: initial commit

---
+++ src/test/java/com/etechoracio/pw2/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etechoracio.pw2.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
