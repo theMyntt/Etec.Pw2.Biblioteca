238b8fc478020265eccd8a38c4b5beacda51efea
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-01 23:41:52 -0300

    feat: search by user

--- src/main/java/com/etechoracio/pw2/biblioteca/repositories/IEmprestimoRepository.java
+++ src/main/java/com/etechoracio/pw2/biblioteca/repositories/IEmprestimoRepository.java
@@ -4,6 +4,9 @@ import com.etechoracio.pw2.biblioteca.models.EmprestimoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IEmprestimoRepository extends JpaRepository<EmprestimoModel, Long> {
+    List<EmprestimoModel> findByUsuarioIgnoreCase(String nome);
 }
