cec8d66747e3dd98bd1c079fdb061161461828e0
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-02 00:01:21 -0300

    fix: missing post annotation

--- src/main/java/com/etechoracio/pw2/biblioteca/controllers/LivroController.java
+++ src/main/java/com/etechoracio/pw2/biblioteca/controllers/LivroController.java
@@ -4,6 +4,7 @@ import com.etechoracio.pw2.biblioteca.models.LivroModel;
 import com.etechoracio.pw2.biblioteca.services.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class LivroController {
     @Autowired
     private LivroService service;
 
+    @PostMapping
     public ResponseEntity<LivroModel> Create(@RequestBody LivroModel model) throws Exception {
         return service.CriarLivroAsync(model);
     }
